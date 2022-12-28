69832016a9f04212b36f1a22f995ef8339748db6
KIM MINJI <sml07113@gmail.com>
2022-12-29 08:26:40 +0900

    feat : 로그아웃 기능 구현

--- sonagi/src/pages/QnAPage.jsx
+++ sonagi/src/pages/QnAPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axiosInstance from "../axiosInstance";
 import "./QnAPage.css";
 
@@ -28,16 +28,17 @@ export default function QnAPage(props) {
       <p className="sehth">
         {id}번째 기본 질문<br></br>
         {content}
-        <button className="버튼">
-          <svg className="아이콘" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <button>
+          <Link to="/comment">댓글</Link>
+          {/* <svg className="아이콘" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M256 448c141.4 0 256-93.1 256-208S397.4 32 256 32S0 125.1 0 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9c-5.5 9.2-11.1 16.6-15.2 21.6c-2.1 2.5-3.7 4.4-4.9 5.7c-.6 .6-1 1.1-1.3 1.4l-.3 .3 0 0 0 0 0 0 0 0c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c28.7 0 57.6-8.9 81.6-19.3c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9zM128 272c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32zm128 0c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32zm160-32c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z" />
-          </svg>
+          </svg> */}
         </button>
       </p>
       <table className="tabAns">
         <tr className="qtr">
           <td classname="qtd">
-            <span>2022</span>
+            <span>2019</span>
             <br></br>
             <br></br>
             아부지 : 가정과. 사회에. 책임을. 다한다. <br></br>
@@ -46,7 +47,7 @@ export default function QnAPage(props) {
             막냉 : ISTJ라서 말이 없다.
           </td>
           <td classname="qtd">
-            <span>2023</span>
+            <span>2020</span>
             <br></br>
             <br></br>
             아부지 : 오래오래. 우리가족. 언제든. 따뜻하게. <br></br>
@@ -57,7 +58,7 @@ export default function QnAPage(props) {
         </tr>
         <tr className="qtr">
           <td classname="qtd">
-            <span>2024</span>
+            <span>2021</span>
             <br></br>
             <br></br>
             아부지 : 든든하고. 건강한. 대한의. 사나이. <br></br>
@@ -66,7 +67,7 @@ export default function QnAPage(props) {
             막냉 : 스트레이키즈 짱!!
           </td>
           <td classname="qtd">
-            <span>2025</span>
+            <span>2022</span>
             <br></br>
             <br></br>
             아부지 : 자녀들. 무탈하고. 오래오래. 건강하게.<br></br>
